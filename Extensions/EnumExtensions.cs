766085e3dd3d0fd8a50621414720abc0d9d9a3e2
SYLVIO ALBERNAZ MACHADO NETO <saneto2@latam.stefanini.com>
2025-09-16 10:37:37 -0300

    Alterar namespace e atualizar diretivas em views
    Mudança do namespace da classe `EnumExtensions` de `TestePositivo.Domain.Extensions` para `TestePositivo.Extensions`, refletindo uma reorganização do código.
    
    Adição da diretiva `@using TestePositivo.Extensions` em arquivos `.cshtml` (Delete.cshtml, Details.cshtml e Index.cshtml) para facilitar o uso das funcionalidades do novo namespace, melhorando a legibilidade e manutenção do código.

--- Extensions/EnumExtensions.cs
+++ Extensions/EnumExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
-namespace TestePositivo.Domain.Extensions;
+namespace TestePositivo.Extensions;
 
 public static class EnumExtensions
 {
